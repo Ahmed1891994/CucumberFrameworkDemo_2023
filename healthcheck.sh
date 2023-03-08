efa8a20c0e9e66256a28a1a8fd73e6fa90b918c6
Ahmed1891994 <ahmedhakem189@yahoo.com>
2023-03-08 15:33:37 +0200

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -9,7 +9,7 @@ while [ "$( curl -s http://$HUBHOST:4444/wd/hub/status | jq -r .value.ready )" !
 do
 	sleep 1
 done
-
+sleep 1000000
 # start the java command
 java -cp framework_jar.jar:framework_jar-tests.jar:libs/* -DHUBHOST=$HUBHOST -Denv=prod org.testng.TestNG $MODULE
     
